ed3fa497217bd4dbef838a8629ddce3209d0946e
RyuSangwon <rsw2@connectly.co.kr>
2025-07-02 11:38:02 +0900

    first commit

---
+++ src/main/java/com/ryuqq/configImporter/ConfigImporterApplication.java
@@ -0,0 +1,13 @@
+package com.ryuqq.configImporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigImporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigImporterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ryuqq/configImporter/ConfigImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryuqq.configImporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigImporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
